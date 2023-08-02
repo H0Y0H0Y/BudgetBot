e225a4f0753c10203363d82c333dad9c4c5e7da7
H0Y0H0Y <40562335+theLastAirVendor@users.noreply.github.com>
2023-08-02 22:25:08 +0800

    updates

--- bot.py
+++ bot.py
@@ -7,5 +7,14 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Hello there!")
 
+@bot.message_handler(commands=['getChatId'])
+def get_chat_id(message):
+    bot.reply_to(message, message.chat.id)
+
+# handle message with that can be a float or number
+@bot.message_handler(func=lambda message: message.text.isdigit() or message.text.replace('.', '', 1).isdigit())
+def handle_number(message):
+    bot.reply_to(message, "You sent a number")
+
 if __name__ == '__main__':
     bot.infinity_polling()
