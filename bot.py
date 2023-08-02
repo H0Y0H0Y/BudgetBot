1dc3d418a4bf2aa724dad615315caebd275eb459
H0Y0H0Y <40562335+theLastAirVendor@users.noreply.github.com>
2023-08-02 21:54:32 +0800

    added bot setup

---
+++ bot.py
@@ -0,0 +1,8 @@
+import telebot
+from constants import BOT_TOKEN
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hello there!")
--- constants.py
+++ constants.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 MONGODB_CONN_STRING = os.getenv("MONGODB_CONN_STRING")
 DB = os.getenv("DB")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
